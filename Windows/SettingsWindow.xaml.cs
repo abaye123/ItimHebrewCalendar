2ef51604cc72ac648ef0de5bcad529648db86782
abaye <8140hp@gmail.com>
2026-05-17 04:09:59 +0300

    v1.7.0

--- Windows/SettingsWindow.xaml.cs
+++ Windows/SettingsWindow.xaml.cs
@@ -212,11 +212,11 @@ namespace ItimHebrewCalendar.Windows
                 var version = asm.GetName().Version;
                 AboutVersionText.Text = version != null
                     ? $"גרסה {version.Major}.{version.Minor}.{version.Build}"
-                    : "גרסה 1.6.0";
+                    : "גרסה 1.7.0";
             }
             catch
             {
-                AboutVersionText.Text = "גרסה 1.6.0";
+                AboutVersionText.Text = "גרסה 1.7.0";
             }
 
             try
